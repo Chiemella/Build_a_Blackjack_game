cf6c2c3e0989250e1925847e682b52fe8f4b419f
chiemella <williamschiemelaod@gmail.com>
2024-10-06 06:51:43 +0100

    Create the save feature: updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+console.log(saveEl)
 function increment() {
     // Change this to use the plus equal technique you've learned
     count += 1
@@ -8,5 +10,8 @@ function increment() {
 }
 
 function save() {
+   let countStr = count + " - "
+    saveEl.innerText = countStr
+
     console.log(count)
 }
